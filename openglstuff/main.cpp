acde7d991044bc39947531e916ef8d046adc6cc0
StephenThomasUWTSD <stephen.skarzynski@googlemail.com>
2016-05-04 20:13:07 +0100

    initial commit
    this is the initial commit for my pairs matching tiles game.
    so far tiles have been created that fill a vector of structs.
    
    TODO: add pairs of unique random numbers to the struct without
    overwriting the xyz coords.

---
+++ openglstuff/main.cpp
@@ -0,0 +1,244 @@
+#include <stdlib.h>
+#include <glut.h>
+#include <iostream>
+#include <gl\GL.h>
+#include <gl\GLU.h>
+#include <math.h>
+#include <cmath>
+#include <vector>
+
+
+using namespace std;
+
+const int amount = 32;
+
+struct Box {
+	GLfloat x, y, z;
+	GLfloat red, green, blue;
+};
+
+vector<Box> boxes;
+
+// angle of rotation for the camera direction
+float angle = 0.0f;
+// actual vector representing the camera's direction
+float lx = 0.0f, lz = -1.0f;
+// XZ position of the camera
+float x = 3.0f, z = 1.0f;
+// the key states. These variables will be zero
+//when no key is being presses
+float deltaAngle = 0.0f;
+float deltaMove = 0;
+
+void changeSize(int w, int h) {
+
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window of zero width).
+	if (h == 0)
+		h = 1;
+	float ratio = w * 1.0f / h;
+
+	// Use the Projection Matrix
+	glMatrixMode(GL_PROJECTION);
+
+	// Reset Matrix
+	glLoadIdentity();
+
+	// Set the viewport to be the entire window
+	glViewport(0, 0, w, h);
+
+	// Set the correct perspective.
+	gluPerspective(60.0f, ratio, 1.0f, 1000.0f);
+
+	// Get Back to the Modelview
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void drawBox(float w, float h, float l)
+//drawBox
+//***************************************************************************
+//This function first translates a further x,y,z units then draws a box of
+//width w, height h, length l
+{
+	glPushMatrix();       //save modelview
+	w = w / 2.0; h = h / 2.0; l = l / 2.0; //adjust values so centre is in middle of box
+	//draw faces of box
+	glBegin(GL_POLYGON);
+	glVertex3f(w, l, h);
+	glVertex3f(w, l, -h);
+	glVertex3f(-w, l, -h);
+	glVertex3f(-w, l, h);
+	glEnd();
+	glBegin(GL_POLYGON);
+	glVertex3f(w, l, h);
+	glVertex3f(-w, l, h);
+	glVertex3f(-w, -l, h);
+	glVertex3f(w, -l, h);
+	glEnd();
+	glBegin(GL_POLYGON);
+	glVertex3f(w, l, h);
+	glVertex3f(w, -l, h);
+	glVertex3f(w, -l, -h);
+	glVertex3f(w, l, -h);
+	glEnd();
+	glBegin(GL_POLYGON);
+	glVertex3f(-w, l, h);
+	glVertex3f(-w, l, -h);
+	glVertex3f(-w, -l, -h);
+	glVertex3f(-w, -l, h);
+	glEnd();
+	glBegin(GL_POLYGON);
+	glVertex3f(-w, -l, h);
+	glVertex3f(-w, -l, -h);
+	glVertex3f(w, -l, -h);
+	glVertex3f(w, -l, h);
+	glEnd();
+	glBegin(GL_POLYGON);
+	glVertex3f(w, -l, -h);
+	glVertex3f(-w, -l, -h);
+	glVertex3f(-w, l, -h);
+	glVertex3f(w, l, -h);
+	glEnd();
+	glPopMatrix();   //restore previous modelview matrix so leaving things as you found them 
+}
+
+void drawSnowMan() {
+
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	// Draw Body
+
+	glTranslatef(0.0f, 0.75f, 0.0f);
+	glutSolidSphere(0.75f, 20, 20);
+
+	// Draw Head
+	glTranslatef(0.0f, 1.0f, 0.0f);
+	glutSolidSphere(0.25f, 20, 20);
+
+	// Draw Eyes
+	glPushMatrix();
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glTranslatef(0.05f, 0.10f, 0.18f);
+	glutSolidSphere(0.05f, 10, 10);
+	glTranslatef(-0.1f, 0.0f, 0.0f);
+	glutSolidSphere(0.05f, 10, 10);
+	glPopMatrix();
+
+	// Draw Nose
+	glColor3f(1.0f, 0.5f, 0.5f);
+	glRotatef(0.0f, 1.0f, 0.0f, 0.0f);
+	glutSolidCone(0.08f, 0.5f, 10, 2);
+}
+
+void computePos(float deltaMove) {
+
+	x += deltaMove * lx * 0.1f;
+	z += deltaMove * lz * 0.1f;
+}
+
+void computeDir(float deltaAngle) {
+
+	angle += deltaAngle;
+	lx = sin(angle);
+	lz = -cos(angle);
+}
+
+void renderScene(void) {
+
+	if (deltaMove)
+		computePos(deltaMove);
+	if (deltaAngle)
+		computeDir(deltaAngle);
+
+	// Clear Color and Depth Buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Reset transformations
+	glLoadIdentity();
+	// Set the camera
+	gluLookAt(x, 20.0f, z,
+		x + lx, 3.0f, z + lz,
+		0.0f, 1.0f, 0.0f);
+
+	// Draw ground
+
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glBegin(GL_QUADS);
+	glVertex3f(-100.0f, 0.0f, -100.0f);
+	glVertex3f(-100.0f, 0.0f, 100.0f);
+	glVertex3f(100.0f, 0.0f, 100.0f);
+	glVertex3f(100.0f, 0.0f, -100.0f);
+	glEnd();
+
+	// Draw 36 SnowMen
+
+	for (Box box : boxes) {
+		glPushMatrix();
+		glTranslatef(box.x, box.y, box.z);
+		glColor3f(box.red, box.green, box.blue);
+		drawBox(1, 1, 1);
+		glPopMatrix();
+	}
+
+	glutSwapBuffers();
+}
+
+
+void pressKey(int key, int xx, int yy) {
+
+	switch (key) {
+	case GLUT_KEY_LEFT: deltaAngle = -0.01f; break;
+	case GLUT_KEY_RIGHT: deltaAngle = 0.01f; break;
+	case GLUT_KEY_UP: deltaMove = 0.5f; break;
+	case GLUT_KEY_DOWN: deltaMove = -0.5f; break;
+	}
+}
+
+void releaseKey(int key, int x, int y) {
+
+	switch (key) {
+	case GLUT_KEY_LEFT:
+	case GLUT_KEY_RIGHT: deltaAngle = 0.0f; break;
+	case GLUT_KEY_UP:
+	case GLUT_KEY_DOWN: deltaMove = 0; break;
+	}
+}
+
+float randColour() {
+	return (double)rand() / (RAND_MAX + 1.0f);
+}
+
+int main(int argc, char **argv) {
+
+	// init GLUT and create window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Lighthouse3D - GLUT Tutorial");
+
+	// register callbacks
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+	glutIdleFunc(renderScene);
+
+	glutSpecialFunc(pressKey);
+
+	// here are the new entries
+	glutIgnoreKeyRepeat(1);
+	glutSpecialUpFunc(releaseKey);
+
+	// OpenGL init
+	glEnable(GL_DEPTH_TEST);
+
+	for (int i = 0; i < amount / 4; i++) {
+		for (int j = 0; j < amount / 4; j++) {
+			boxes.push_back({ i*1.0f, 0.0f, j*1.0f, randColour(), randColour(), randColour() });
+		}
+	}
+
+	// enter GLUT event processing cycle
+	glutMainLoop();
+
+	return 1;
+}
